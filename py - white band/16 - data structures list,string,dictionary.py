1752d442e2a5da710dd8ba8e8b9549661752c5ba
Y. ROSELINE JENNIFER <154997337+Roselinejennifer@users.noreply.github.com>
2024-06-10 13:48:45 +0530

    Update 16 - data structures list,string,dictionary.py

--- py - white band/16 - data structures list,string,dictionary.py
+++ py - white band/16 - data structures list,string,dictionary.py
@@ -6,8 +6,6 @@ fruits = {
 }
 apple_price = fruits['apple']
 print("An apple costs :", apple_price)
-
-# Now let's say we want to add a new fruit to the dictionary
 fruits['mango'] = 125
 print(fruits)
 
