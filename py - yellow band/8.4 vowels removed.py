0956dc6699a24d513c17947318b316ce827f0edf
Y. ROSELINE JENNIFER <154997337+Roselinejennifer@users.noreply.github.com>
2024-06-10 13:50:02 +0530

    Update 8.4 vowels removed.py

--- py - yellow band/8.4 vowels removed.py
+++ py - yellow band/8.4 vowels removed.py
@@ -1,12 +1,8 @@
 def remove_vowels(input_string):
-    vowels = "aeiouAEIOU"  # Define a string containing all vowels
-
-    # Use list comprehension to create a new string with characters that are not vowels
+    vowels = "aeiouAEIOU"  
     new_string = ''.join([char for char in input_string if char not in vowels])
 
     return new_string
 
-
-# Example usage:
 input_string = "Hello, World!"
 print("String with vowels removed:", remove_vowels(input_string))
