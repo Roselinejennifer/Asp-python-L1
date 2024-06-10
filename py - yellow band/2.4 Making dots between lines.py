8b55364344c0b104c88cda7301be425d9f9476c5
Y. ROSELINE JENNIFER <154997337+Roselinejennifer@users.noreply.github.com>
2024-06-10 13:49:36 +0530

    Update 2.4 Making dots between lines.py

--- py - yellow band/2.4 Making dots between lines.py
+++ py - yellow band/2.4 Making dots between lines.py
@@ -1,8 +1,8 @@
 from turtle import *
 
-step = 6 #Change its values as much as you'd like to
+step = 6 
 speed(50)
-for i in range(400): #Eventually 360 is better sometimes
+for i in range(400):
     forward(i * 3/step + i)
     left(400/step + 0.350)
     hideturtle()
